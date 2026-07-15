350bd1a7b49bb651a81b6fd5d874bdb5209e97a8
Gabriel Frameschi <gabrieldframeschi@gmail.com>
2026-07-14 21:05:13 -0300

    setup: adds Entity Framework to VideoGameCharacterApi

---
+++ VideoGameCharacterApi/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameCharacterApi.Models;
+
+namespace VideoGameCharacterApi.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    // DbSet "=" Table, 'Characters' is the name of the Table
+    public DbSet<Character> Characters => Set<Character>();
+}
